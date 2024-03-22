a765d29768d59ee02ab84bb346e732472be0886c
Shivam Singh <roxshivamsingh@gmail.com>
2024-03-22 16:15:27 +0530

    FIX: Navbar;

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,53 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+
+
+const NAVLINKS = ['Home', 'Projects', 'Contact Us'];
+
+export default function NavBar() {
+
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar component="nav" elevation={0} color='inherit'>
+                <Toolbar >
+                    <Link href=""
+                        sx={{
+                            flexGrow: 1,
+                            color: "inherit",
+                            textDecoration: "none",
+                            fontFamily: 'Pacifico',
+                            "&:hover": {
+                                textDecoration: "underline",
+                            },
+                            fontSize: { lg: 30, sm: 20 }
+                        }}
+
+                    >
+                        roxshivamsingh
+
+                    </Link>
+                    <Box sx={{ ml: 'auto' }}>
+                        {NAVLINKS.map((item) => (
+                            <Button key={item} sx={{
+                                color: 'inherit',
+                                fontWeight: 700,
+                                textTransform: "none",
+                            }}
+                                disableElevation
+                                disableRipple
+                            >
+                                {item}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -1,40 +1,7 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-
-
-const navItems = ['Home', 'Projects', 'Contact Us'];
+import NavBar from '../../components/navbar/navbar';
 
 export default function Home() {
-
-
-
     return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar component="nav" elevation={0} color='inherit'>
-                <Toolbar >
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1, fontFamily: 'Pacifico' }}
-                    >
-                        roxshivamsingh
-
-                    </Typography>
-                    <Box sx={{ ml: 'auto' }}>
-                        {navItems.map((item) => (
-                            <Button key={item} sx={{ color: 'inherit', fontWeight: 700, textTransform: "none" }}>
-                                {item}
-                            </Button>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <NavBar />
     );
 }
