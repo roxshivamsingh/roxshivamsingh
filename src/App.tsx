e307e9b1d0d45fbbd3a78d958b9170a05ac5f071
Shivam Singh <roxshivamsingh@gmail.com>
2024-03-22 16:02:43 +0530

    ADD: Navbar;

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Home from './containers/home/home';
 
 function App() {
 
   return (
     <div>
-      Hello world!
+      <Home />
     </div>
   )
 }
