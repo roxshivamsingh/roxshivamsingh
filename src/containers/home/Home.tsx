de3572fb12243cbe5bfdf53ef91434aae8cb4d5a
Shivam Singh <roxshivamsingh@gmail.com>
2024-05-25 18:28:53 +0530

    ADD: Download resume option;

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -1,26 +1,18 @@
 import classes from "./Home.module.scss";
 import EmojiBullet from "../../components/emoji-bullet/emoji-bullet";
 import passport from "../../assets/images/photo.png";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { info } from "../../types/info";
-import { useTheme } from "@mui/material/styles";
+import { SxProps, useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { Link } from "react-router-dom";
 import { Iconify } from "../../components/iconify";
+import HtmlTooltip from "../../components/html-tooltip";
 export default function Home() {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
-  return (<Box
-    component="main"
-    sx={{
-      display: "flex",
-      flexDirection: { xs: "column", md: "row" },
-      alignItems: "center",
-      justifyContent: "center",
-      minHeight: "calc(100vh - 70px)"
-    }}
-  >
+  return (<Box component="main" sx={SX.Container}>
     <Box
       className={[classes.avatar, classes.shadowed, classes.floating].join(' ')}
       alt={"image of developer"}
@@ -67,8 +59,40 @@ export default function Home() {
         <Link target="_blank" to={info.social.github} >
           <Iconify icon="line-md:github-loop" width={40} />
         </Link>
+
+        <HtmlTooltip
+          title={
+            <>
+              <Typography fontFamily='Pacifico' gutterBottom>Download Resume</Typography>
+              <b>Name:</b> <em>shivam-singh-nodejs.pdf</em>
+              <br />
+              <b>{"Updated At:"}</b> <em>{'25/05/2024, at 10:00 PM'}</em>
+            </>
+          }
+        >
+          <Link target="_blank" to='' >
+            <Iconify icon='prime:file-pdf' width={40} sx={SX.IconResume} />
+          </Link>
+        </HtmlTooltip>
+
       </Box>
     </Box>
-  </Box>
+  </Box >
   );
+
+}
+
+const SX: { [key: string]: SxProps } = {
+  Container: {
+    display: "flex",
+    flexDirection: { xs: "column", md: "row" },
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "calc(100vh - 70px)"
+  },
+  IconResume: {
+    "&:hover": {
+      color: "red"
+    }
+  }
 }
