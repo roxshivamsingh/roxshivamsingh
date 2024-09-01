c9aacd3d07e12844bac16b0b82716483b8759267
Shivam Singh <roxshivamsingh@gmail.com>
2024-09-01 17:49:30 +0530

    ADD: Timeline (experience) component;

--- src/containers/about/About.tsx
+++ src/containers/about/About.tsx
@@ -80,18 +80,18 @@ export default function About() {
           About
         </Typography>
         <Box sx={{ mt: "3rem" }}>
+
           {[renderAboutText, renderSkillText, renderMiscText]?.map((row, i) => {
             return (<Terminal key={i}>
               {row}
             </Terminal>)
           })}
+          <Typography variant='h4' sx={{ fontWeight: 500, mb: 1 }}>
+            Experience
+          </Typography>
+          <AboutTimeline />
 
         </Box>
-        <Typography variant='h4' sx={{ fontWeight: 500, mb: 1 }}>
-          Experience
-        </Typography>
-        <AboutTimeline />
-
       </Container>
 
     </>
